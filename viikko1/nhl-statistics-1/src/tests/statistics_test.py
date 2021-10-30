48d9a26b69d2264d824e94c626be0f7f0e7b7cc9
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2021-10-30 14:09:38 +0000

    add first batch of tests

---
+++ viikko1/nhl-statistics-1/src/tests/statistics_test.py
@@ -0,0 +1,43 @@
+import unittest
+from statistics import Statistics
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatistics(unittest.TestCase):
+    def setUp(self):
+        # annetaan Statistics-luokan oliolle "stub"-luokan olio
+        self.statistics = Statistics(
+            PlayerReaderStub()
+        )
+
+    def test_search_with_full_name_returns_matching_player(self):
+        player = self.statistics.search("Kurri")
+        self.assertEquals(player.name, "Kurri")
+
+    def test_search_with_partial_name_returns_matching_player(self):
+        player = self.statistics.search("men")
+        self.assertEquals(player.name, "Semenko")
+
+    def test_search_nonexistent_player(self):
+        player = self.statistics.search("Mörkö")
+        self.assertIsNone(player)
+
+    def test_search_single_player_team(self):
+        players = self.statistics.team("PIT")
+        self.assertEquals(len(players), 1)
+        
+        player = players[0]
+        self.assertEquals(player.name, "Lemieux")
+
+    def test_search_bigger_team(self):
+        players = self.statistics.team("EDM")
+        self.assertEquals(len(players), 3)
